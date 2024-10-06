dca365878bbe129e11918a68c91fa5faa3a3b781
jongman <61715265+godxxy1229@users.noreply.github.com>
2024-10-06 23:01:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let isResponseGenerating = false;
 
 // API configuration
 const API_KEY = atob(encodedAPIKey);
-const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${API_KEY}`;
+const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${API_KEY}`;
 
 // Load theme and chat data from local storage on page load
 const loadDataFromLocalstorage = () => {
